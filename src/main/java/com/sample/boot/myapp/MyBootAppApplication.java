6475a94e445a2d376414338be0b4593c5b7e63e5
sizukutamago <tanaka@tam-bourine.co.jp>
2019-11-01 19:26:09 +0900

    init

---
+++ src/main/java/com/sample/boot/myapp/MyBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.sample.boot.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBootAppApplication.class, args);
+	}
+
+}
